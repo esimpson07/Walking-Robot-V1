ab9dd25d6dcc800a0eed94b2a7d5af3ae2eebd6d
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2021-12-19 21:01:03 -0500

    Update servowalk.ino
    Added turn feature

--- servowalk.ino
+++ servowalk.ino
@@ -3,40 +3,49 @@
 HCPCA9685 HCPCA9685(i2cport);
 
 double s1c = 90;
-double s1t = 45;
+double s1t = 55;
 double s2c = 90;
 double s2t = 90;
 double s3c = 90;
 double s3t = 90;
 double s4c = 90;
-double s4t = 45;
+double s4t = 55;
 double s5c = 90;
 double s5t = 90;
 double s6c = 90;
 double s6t = 90;
 double s7c = 90;
-double s7t = 45;
+double s7t = 55;
 double s8c = 90;
 double s8t = 90;
 double s9c = 90;
 double s9t = 90;
 double s10c = 90;
-double s10t = 45;
+double s10t = 60;
 double s11c = 90;
 double s11t = 90;
 double s12c = 90;
 double s12t = 90;
 
-double servoSpeed = 2;
-double distance = 35;
+double servoSpeed = 10; //orignally 2
+double distance = 45; // orignally 35
 long cycle = 0;
 long pastTime = 0;
 long currentTime = 0;
-long delayTime = 25;
-int sChange = 1.5;
+long delayTime = 3;
+int sChange = 1.5; // originally 1.5
 int sHeight = 50;
-int sRaise = sHeight + 30;
+int sRaise = sHeight + 35;
 int sMid = sHeight;
+int sTurnAngle = 60;
+int turn = 0; // originally 30
+
+//All s1, s2, s3 etc. all are servos on the robot
+//s1, s4, s7, and s10 are all the turn servos
+//s2, s5, s8, s11 are first joint servos
+//s3, s6, s9, s12 are all foot actuation servos
+//All servo walk methods are called in the loop to add the number to the servo postion
+//Servoupdate tells the servos to all move in the PCA9685 servo driver
 
 void s1walk(){
   if (s1c > s1t + servoSpeed){
@@ -239,57 +248,81 @@ int c5 = 90;*/
 void FRmove(){
   if(cycle >= 0 && cycle < 13){
     s2t = sRaise;
+    s1t = sTurnAngle - (turn * 2);
   } else if(cycle >= 13 && cycle < 25){
     s3t = sMid - (distance * sChange);
+    s1t = sTurnAngle + (turn * 2);
   } else if(cycle >= 25 && cycle < 50){
     s2t = sHeight;
-  } else if(cycle >= 100 && cycle < 125){
+  } else if(cycle >= 75 && cycle < 100){
+    s1t = sTurnAngle + turn;
+  }else if(cycle >= 100 && cycle < 125){
     s3t = sMid;
+    s1t = sTurnAngle;
   } else if(cycle >= 150 && cycle < 175){
     s3t = sMid + distance;
+    s1t = sTurnAngle - turn;
   }
 }
 
 void FLmove(){
   if(cycle < 25 && cycle >= 0){
     s6t = sMid;
+    s4t = sTurnAngle;
   } else if(cycle >= 50 && cycle < 75){
     s6t = sMid + distance;
+    s4t = sTurnAngle + turn;
   } else if(cycle >= 100 && cycle < 113){
     s5t = sRaise;
+    s4t = sTurnAngle + (turn * 2);
   } else if(cycle >= 113 && cycle < 125){
     s6t = sMid - (distance * sChange);
+    s4t = sTurnAngle - (turn * 2);
   } else if(cycle >= 125 && cycle < 150){
     s5t = sHeight;
-  }
-}
-
-void BLmove(){
-  if(cycle >= 0 && cycle < 25){
-    s12t = sMid - distance;
-  } else if(cycle >= 50 && cycle < 63){
-    s11t = sRaise;
-  } else if(cycle >= 63 && cycle < 75){
-    s12t = sMid + (distance * sChange);
-  } else if(cycle >= 75 && cycle < 100){
-    s11t = sHeight;
   } else if(cycle >= 150 && cycle < 175){
-    s12t = sMid;
+    s4t = sTurnAngle - turn;
   }
 }
 
 void BRmove(){
   if(cycle >= 175 && cycle < 200){
     s8t = sHeight; //70
+  } else if(cycle >= 0 && cycle < 25){
+    s7t = sTurnAngle - turn;
   } else if(cycle >= 50 && cycle < 75){
     s9t = sMid; //90
+    s7t = sTurnAngle;
   } else if(cycle >= 100 && cycle < 125){
     s9t = sMid - distance; //45
+    s7t = sTurnAngle + turn;
   } else if(cycle >= 150 && cycle < 163){
     s8t = sRaise; //135
+    s7t = sTurnAngle + (turn * 2);
   } else if(cycle >= 163 && cycle < 175){
     s9t = sMid + (distance * sChange); //135
-    
+    s7t = sTurnAngle - (turn * 2);
+  }
+}
+
+void BLmove(){
+  if(cycle >= 0 && cycle < 25){
+    s12t = sMid - distance;
+    s10t = sTurnAngle - turn;
+  } else if(cycle >= 50 && cycle < 63){
+    s11t = sRaise;
+    s10t = sTurnAngle - (turn * 2);
+  } else if(cycle >= 63 && cycle < 75){
+    s12t = sMid + (distance * sChange);
+    s10t = sTurnAngle + (turn * 2);
+  } else if(cycle >= 75 && cycle < 100){
+    s11t = sHeight;
+  } else if(cycle >= 100 && cycle < 125){
+    s9t = sTurnAngle;
+    s10t = sTurnAngle + turn;
+  } else if(cycle >= 150 && cycle < 175){
+    s12t = sMid;
+    s10t = sTurnAngle;
   }
 }
 
