8e31d8500ed51951bc3ddc7725d4d9b8002683f5
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2021-12-26 10:49:22 -0500

    Update servowalk.ino

--- servowalk.ino
+++ servowalk.ino
@@ -1,6 +1,34 @@
 #include "HCPCA9685.h"
 #define  i2cport 0x40
+#include <Wire.h>
+#include <MPU6050.h>
+#include <ESP8266WiFi.h>
+#define SERVERMODE 0
+#define BAUD_SERIAL 115200
+#define RXBUFFERSIZE 1024
+#define STACK_PROTECTOR  512 // bytes
+#define MAX_SRV_CLIENTS 2
+
+MPU6050 mpu;
 HCPCA9685 HCPCA9685(i2cport);
+
+const int port = 23;
+WiFiServer server(port);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+#if SERVERMODE
+const char* ssid = "PandoraAccessPoint";
+const char* password = "b8070ecd10cd20";
+#else
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+String id = "ESPsoftAP_10";
+String pw = "Soft_AP_PW";
+#endif
+String sbuf;
+double val1;
+double val2;
 
 double s1c = 90;
 double s1t = 55;
@@ -28,18 +56,19 @@ double s12c = 90;
 double s12t = 90;
 
 double wSpeed = 0;
-double servoSpeed = 180; //orignally 2
-double distance = 0; // orignally 35
+double tSpeed = 0;
+double servoSpeed = 6 * (abs(wSpeed) + 0.1); //orignally 2
+double distance = wSpeed * 35;
+long delayTime = 15 - (abs(wSpeed) * 14);
 long cycle = 0;
 long pastTime = 0;
 long currentTime = 0;
-long delayTime = 2;
 int sChange = 1.5; // originally 1.5
 int sHeight = 50;
 int sRaise = sHeight + 35;
 int sMid = sHeight;
 int sTurnAngle = 55;
-int turn = 0; // originally 30
+int turn = tSpeed * 60;
 
 //All s1, s2, s3 etc. all are servos on the robot
 //s1, s4, s7, and s10 are all the turn servos
@@ -319,7 +348,6 @@ void BLmove(){
   } else if(cycle >= 75 && cycle < 100){
     s11t = sHeight;
   } else if(cycle >= 100 && cycle < 125){
-    s9t = sTurnAngle;
     s10t = sTurnAngle + turn;
   } else if(cycle >= 150 && cycle < 175){
     s12t = sMid;
@@ -359,16 +387,155 @@ void setup() {
   Serial.setTimeout(5);
   HCPCA9685.Init(SERVO_MODE);
   HCPCA9685.Sleep(false);
+    // Initialize MPU6050
+  Serial.println("Initialize MPU6050");
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+  mpu.calibrateGyro();
+  mpu.setThreshold(3);
+  checkSettings();
+  
+  #if SERVERMODE
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  
+  Serial.println();
+  Serial.print("connected, address=");
+  Serial.println(WiFi.localIP());
+  #else
+  Serial.print("Setting soft-AP ... ");
+  boolean result = WiFi.softAP(id, pw);
+  if(result == true)
+  {
+    WiFi.softAPConfig(local_ip, gateway, subnet);
+    Serial.println("Wifi Ready");
+  }
+  else
+  {
+    Serial.print("WiFi Failed: ");
+    Serial.println(result);
+  }
+
+  #endif
+
+  server.begin();
+  server.setNoDelay(true);
+}
+
+void checkSettings()
+{
+  Serial.println();
+  
+  Serial.print(" * Sleep Mode:        ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+  
+  Serial.print(" * Clock Source:      ");
+  switch(mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+  
+  Serial.print(" * Gyroscope:         ");
+  switch(mpu.getScale())
+  {
+    case MPU6050_SCALE_2000DPS:        Serial.println("2000 dps"); break;
+    case MPU6050_SCALE_1000DPS:        Serial.println("1000 dps"); break;
+    case MPU6050_SCALE_500DPS:         Serial.println("500 dps"); break;
+    case MPU6050_SCALE_250DPS:         Serial.println("250 dps"); break;
+  } 
+  
+  Serial.print(" * Gyroscope offsets: ");
+  Serial.print(mpu.getGyroOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getGyroOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getGyroOffsetZ());
+  
+  Serial.println();
+}
+
+void sRun(){
+  if (server.hasClient()) {
+    for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
+      if (!serverClients[i]) {
+        serverClients[i] = server.available();
+        Serial.print("New client idx: ");
+        Serial.println(i);
+        break;
+      }
+
+      if (i >= MAX_SRV_CLIENTS) {
+        server.available().println("no sessions available");
+        Serial.print("Server exceeded max connections: ");
+        Serial.println(MAX_SRV_CLIENTS);
+      }
+    }
+  }
+
+  for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
+    while (serverClients[i].available()) {
+      size_t maxToSerial = std::min(serverClients[i].available(), Serial.availableForWrite());
+      maxToSerial = std::min(maxToSerial, (size_t)STACK_PROTECTOR);
+      uint8_t buf[maxToSerial];
+      size_t tcp_got = serverClients[i].read(buf, maxToSerial);
+
+      sbuf = String((char*)buf);
+
+      //serverClients[i].println("0");
+      if (sbuf.length() > 0 && sbuf.substring(0,1) != 0) {
+        val1 = sbuf.substring(0,2).toInt();
+        val2 = sbuf.substring(2,4).toInt();
+        wSpeed = (val1 - 40) / 40;
+        tSpeed = (val2 - 40);
+        serverClients[i].print("walkSpeed = ");
+        serverClients[i].println(wSpeed);
+        serverClients[i].print("wSpeed = ");
+        serverClients[i].print("turnspeed = ");
+        serverClients[i].println(tSpeed);
+        Serial.println(wSpeed);
+        Serial.println(val1);
+        Serial.println((val1 - 40) / 40);
+      }
+
+      
+    }
+  }
 }
 
 void loop() {
+  servoSpeed = 6 * (abs(wSpeed) + 0.1); //orignally 2
+  distance = wSpeed * 35;
+  delayTime = 15 - (abs(wSpeed) * 14);
+  turn = tSpeed;
+  if(wSpeed < 0){
+    sChange = 1;
+    if(distance > 20){
+      distance = 20;
+    }
+  } else {
+    sChange = 1.5;
+  }
+  sRun();
   currentTime = millis();
   servoUpdate();
   FRmove();
   FLmove();
   BRmove();
   BLmove();
-  Serial.println(cycle);
   if (pastTime + delayTime <= currentTime){
     cycle ++;
     if(cycle > 200){
@@ -376,4 +543,12 @@ void loop() {
     }
     pastTime = currentTime;
   }
+  
+  /*Vector rawGyro = mpu.readRawGyro();
+  Serial.print(" Xraw = ");
+  Serial.print(rawGyro.XAxis);
+  Serial.print(" Yraw = ");
+  Serial.print(rawGyro.YAxis);
+  Serial.print(" Zraw = ");
+  Serial.println(rawGyro.ZAxis);*/
 }
