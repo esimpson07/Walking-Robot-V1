ac9a43e939caf3e92116a299d5c848fb7dc47d35
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-01-02 00:10:38 -0500

    Update and rename telnettest123120212.py to messagenode.py
    Finished ros code for the first time, works on the walking robot and the walking robot is controlled by a logitech joystick

---
+++ messagenode.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.8
+from telnetlib import Telnet
+import roslib; roslib.load_manifest('wsjs')
+import rospy
+import tf.transformations
+from geometry_msgs.msg import Twist
+x = 0
+y = 0
+
+tn = Telnet('192.168.1.1',23)
+
+def callback(msg):
+    rospy.loginfo("Received a /cmd_vel message!")
+    rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
+    rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+    x = msg.linear.x + 0.03
+    y = msg.angular.z
+    cats = bytes(str(str(int((x * 8) + 4)) + str(int((y * 8) + 4))), 'utf-8')
+    cats2 = (str(str(int((x * 8) + 4)) + str(int((y * 8) + 4))))
+    rospy.loginfo("cats = [%s]"%cats2)
+    tn.write(cats)
+    rospy.loginfo("Telnet interacting!:[%s]"%cats)
+    
+
+def listener():
+    rospy.init_node('cmd_vel_listener')
+    rospy.Subscriber("/input_joy/cmd_vel", Twist, callback)
+    tn = Telnet('192.168.1.1',23)
+
+    
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
